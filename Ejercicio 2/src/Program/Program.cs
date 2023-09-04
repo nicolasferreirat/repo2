35fea489bcc3a61020967d82610c58a542f6992c
Nicolas Ferreira <nicolas.ferreira@correo.ucu.edu.uy>
2023-09-04 00:33:02 -0300

    Ejercicio 2 modificado

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,7 +7,7 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand");
+            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555",DateTime.Now, "Wall Street", "Armand");
             Console.WriteLine(appointmentResult);
 
             string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "", "5555-555-555", DateTime.Now, "Queen Street", "");
@@ -15,3 +15,14 @@ namespace Program
         }
     }
 }
+
+
+
+
+
+/*En base al principio SRP, la clase Appointment no cumple con el, ya 
+que tiene varias responsabilidades para cumplir, entre ellas: validar los datos y crear un mensaje sobra la consulta.
+
+En base al patrón Expert, lo ideal en esta clase es dividirla en dos, separando la responsabilidad de
+validar los datos en una propia clase separada.
+Realizado junto a Lucas Rodriguez y Francisco Lima */
