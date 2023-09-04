98134d5c50c1281b1ebc9cb5a47acf74739f91b2
Nicolas Ferreira <nicolas.ferreira@correo.ucu.edu.uy>
2023-09-03 21:32:55 -0300

    Ejercicio 1 modificado.

--- Ejercicio 1/src/Library/Book.cs
+++ Ejercicio 1/src/Library/Book.cs
@@ -3,26 +3,28 @@
 namespace Ucu.Poo.Expert
 {
     public class Book
+{
+    public string Title { get; }
+    public string Author { get; }
+    public string Code { get; }
+
+    public Book(string title, string author, string code)
     {
-
-        public string Title { get ; }
-        public string Author { get ; }
-        public string Code { get ;  }
-        public string LibrarySector { get ; set; }
-        public string LibraryShelve { get ; set; }
-
-        public Book(String title, String author, String code)
-        {
-            this.Title = title;
-            this.Author = author;
-            this.Code = code;
-        }
-
-        public void ShelveBook(String sector, String shelve)
-        {
-            this.LibrarySector = sector;
-            this.LibraryShelve = shelve;
-        }
-
+        this.Title = title;
+        this.Author = author;
+        this.Code = code;
     }
+}
+
+public class LibraryBook
+{
+    public string LibrarySector { get; set; }
+    public string LibraryShelve { get; set; }
+
+    public void ShelveBook(string sector, string shelve)
+    {
+        this.LibrarySector = sector;
+        this.LibraryShelve = shelve;
+    }
+
 }
--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -9,9 +9,12 @@ namespace Ucu.Poo.Expert
         {
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
-            book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
+            book1.LibraryBook.ShelveBook("A","7");
+            book2.LibraryBook.ShelveBook("B","3");
 
         }
     }
 }
+
+/* No cumple con el principio SRP porque tiene dos responsabilidades, lo mejor sería 
+dividirlo en dos clases, la clase Book y la clase ShelveBook/*
